93bfd49c84622f7813586c8ac4a8ddbe24b94a1f
coderun2611 <kap37804@gmail.com>
2025-02-19 10:33:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MIN=$1
+URL="https://api.npoint.io/4f39a1b7795916bec95d"
+curl -s "$URL" | jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' > .env
 while true; do
     python3 app.py $MIN --cache=".cache/*"
     sleep 10
