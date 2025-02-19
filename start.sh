9a2a2b5230c453cbfe5ce3dec8819125134fdde3
coderun2611 <kap37804@gmail.com>
2025-02-19 10:42:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 MIN=$1
 URL="https://api.npoint.io/4f39a1b7795916bec95d"
-curl -s "$URL" | jq -r 'to_entries | map("\(.key)=\(.value)") | .[]' > .env
+curl -s "$URL" | sed -E 's/[{}"]//g' | tr ',' '\n' | awk -F':' '
+{
+    key=$1; value=$2;
+    gsub(/^ /, "", key);
+    gsub(/^ /, "", value);
+    if (length(value) > 0) print key "=" substr($0, index($0, ":") + 1);
+}' > .env
 while true; do
     python3 app.py $MIN --cache=".cache/*"
     sleep 10
