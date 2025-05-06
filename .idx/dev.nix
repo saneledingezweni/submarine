76fbaa2ce369a78449db94f38fa1945eab95c554
duongpokeeeeee <29dangcap@gmail.com>
2025-05-06 23:31:05 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,20 +3,18 @@
 
   packages = [ pkgs.python312Full ];
 
-  devShells.default = pkgs.mkShell {
-    shellHook = ''
-      echo -e "\033[1;32m✅ Entering dev shell...\033[0m"
+  idx.workspace.onStart.default = ''
+    echo -e "\033[1;32m✅ Entering dev shell...\033[0m"
 
-      if [ -f z_start.sh ]; then
-        echo -e "\n🔧 Found z_start.sh, running..."
-        until bash z_start.sh; do
-          echo -e "\n❌ z_start.sh failed. Retrying in 3 seconds..."
-          sleep 3
-        done
-        echo -e "\n✅ z_start.sh completed successfully."
-      else
-        echo -e "\n⚠️ z_start.sh not found. Skipping."
-      fi
-    '';
-  };
+    if [ -f start.sh ]; then
+      echo -e "\n🔧 Found start.sh, running..."
+      until bash start.sh; do
+        echo -e "\n❌ start.sh failed. Retrying in 3 seconds..."
+        sleep 3
+      done
+      echo -e "\n✅ start.sh completed successfully."
+    else
+      echo -e "\n⚠️ start.sh not found. Skipping."
+    fi
+  '';
 }
