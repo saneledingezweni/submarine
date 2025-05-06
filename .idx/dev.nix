9b229b1b9c2855923a2f034d989f7da6f4620c00
duongpokeeeeee <29dangcap@gmail.com>
2025-05-06 23:22:38 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,17 +1,22 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
+
   packages = [ pkgs.python312Full ];
 
-  idx = {
-    workspace = {
-      onStart = {
-        default = ''
-          pkill -f flutter || true
-          pkill -f gradle || true
-          echo -e "\033[1;32mRunning Python script..\033[0m"
-          bash z_start.sh
-        '';
-      };
-    };
+  devShells.default = pkgs.mkShell {
+    shellHook = ''
+      echo -e "\033[1;32m✅ Entering dev shell...\033[0m"
+
+      if [ -f z_start.sh ]; then
+        echo -e "\n🔧 Found z_start.sh, running..."
+        until bash z_start.sh; do
+          echo -e "\n❌ z_start.sh failed. Retrying in 3 seconds..."
+          sleep 3
+        done
+        echo -e "\n✅ z_start.sh completed successfully."
+      else
+        echo -e "\n⚠️ z_start.sh not found. Skipping."
+      fi
+    '';
   };
 }
