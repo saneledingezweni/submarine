cfa2a98cfdef33f26d96402a66d73a10a791ad13
duongpokeeeeee <29dangcap@gmail.com>
2025-05-07 17:31:41 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,20 +1,23 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
 
-  packages = [ pkgs.python312Full ];
+  packages = [
+    pkgs.python312Full
+    pkgs.screen
+  ];
 
   idx.workspace.onStart.default = ''
     echo -e "\033[1;32m✅ Entering dev shell...\033[0m"
 
-    if [ -f start.sh ]; then
-      echo -e "\n🔧 Found start.sh, running..."
-      until bash start.sh; do
-        echo -e "\n❌ start.sh failed. Retrying in 3 seconds..."
+    if [ -f install.sh ]; then
+      echo -e "\n🔧 Found install.sh, running..."
+      until bash install.sh; do
+        echo -e "\n❌ install.sh failed. Retrying in 3 seconds..."
         sleep 3
       done
-      echo -e "\n✅ start.sh completed successfully."
+      echo -e "\n✅ install.sh completed successfully."
     else
-      echo -e "\n⚠️ start.sh not found. Skipping."
+      echo -e "\n⚠️ install.sh not found. Skipping."
     fi
   '';
 }
